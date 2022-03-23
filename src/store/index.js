256f11f50fa782c416199ef269afce1da5562675
Ahmed samir <tahmsamir288@tutamail.com>
2022-03-23 11:35:36 +0200

    first step

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    param: 3,
+  },
+  getters: {
+    logTest: (state) => (id) => {
+      state.param = id.category_id;
+    },
+  },
+  mutations: {
+    logTest(state, e) {
+      state.param = e;
+      console.log(state.param);
+    },
+  },
+  actions: {
+    logTest(commit, e) {
+      commit.param = e?.category_id;
+    },
+  },
   modules: {},
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li v-for="test in info" :key="test.id" @click="logTest(test)">
+        {{ test.category_id }} : {{ test.category_name }}
+      </li>
+    </ul>
+    <hello-world :param="param" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from "axios";
 import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      info: null,
+      param: null,
+    };
+  },
+  components: { HelloWorld },
+  mounted() {
+    axios
+      .get(
+        "http://magic-tv.live:2095/player_api.php?username=hmaser&password=hmaserv1987&action=get_live_categories"
+      )
+      .then((response) => (this.info = response.data));
+  },
+  methods: {
+    logTest(e) {
+      this.param = e.category_id;
+    },
   },
 };
 </script>
